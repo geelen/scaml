f9ddaa143247b27abbf81d46d49017bad7147f62
Glen Maddern <glenmaddern@gmail.com>
2009-12-31 15:24:00 +1100

    probably not the best way to pass attrs

--- src/main/scala/org/prohax/scaml/dsl/Tag.scala
+++ src/main/scala/org/prohax/scaml/dsl/Tag.scala
@@ -1,12 +1,17 @@
 package org.prohax.scaml.dsl
 
+import scala.collection.immutable.Map
+
 import org.prohax.scaml.Helpers._
 
-class Tag(name: String) {
+class Tag(name: String, attrs: Map[Symbol, String]) {
   def mkString(depth: Int) = {
     indent(depth) + "<" + name + "></" + name + ">"
   }
 }
 object Tag {
-  def apply(s: String) = new Tag(s)
+  def apply(s: String) = new Tag(s, Map[Symbol, String]())
+  def apply(s: String, as: (Symbol, String)*) = {
+    new Tag(s, Map(as:_*))
+  }
 }
